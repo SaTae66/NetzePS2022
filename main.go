9c5c026b6371c207ea310e8e32cd0ea078494e36
mboegl <mathias.boegl@authenticvision.com>
2022-04-26 17:50:21 +0200

    do some crosstesting

--- main.go
+++ main.go
@@ -2,45 +2,39 @@ package main
 
 import (
 	"net"
-	"os"
 )
 
 func main() {
 	remoteAddr := net.UDPAddr{
-		IP:   net.ParseIP("localhost"),
+		IP:   net.ParseIP("10.3.3.140"),
 		Port: 6969,
 		Zone: "",
 	}
+	/*
+		r, err := NewReceiver(512, "down", 10, &remoteAddr)
+		if err != nil {
+			panic(err)
+		}
+
+		fin := make(chan bool, 1)
+
+
+			go func() {
+				err = r.ReceiverFile()
+				if err != nil {
+					panic(err)
+				}
+				fin <- true
+			}()
+	*/
 
 	r, err := NewReceiver(512, "down", 10, &remoteAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	fin := make(chan bool, 1)
-
-	go func() {
-		err = r.ReceiverFile()
-		if err != nil {
-			panic(err)
-		}
-		fin <- true
-	}()
-
-	t, err := NewTransmitter(512, 10)
+	err = r.ReceiverFile()
 	if err != nil {
 		panic(err)
 	}
-
-	f, err := os.Open("data")
-	if err != nil {
-		panic(err)
-	}
-
-	err = t.SendFileTo(f, &remoteAddr)
-	if err != nil {
-		panic(err)
-	}
-
-	<-fin
 }
