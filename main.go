9f6b3a6d19728ef442205c809c1d54b283d99dc1
Bögl Mathias <mathias.boegl@gmail.com>
2022-05-20 22:00:11 +0200

    parse and use cli arguments

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"math"
 	"net"
 	"os"
 	"satae66.dev/netzeps2022/cli"
@@ -16,7 +15,6 @@ type Command interface {
 	Name() string
 
 	Init([]string) error
-	Exec() error
 }
 
 /*
@@ -34,29 +32,19 @@ const flagErrorHandling = flag.ContinueOnError
 */
 
 type DefaultCommand struct {
-	destinationAddress string
-	destinationPort    int
-
 	localAddress string
 	localPort    int
 
 	maxPacketSize     int
 	connectionTimeout int
-
-	debug bool
 }
 
 func (cmd *DefaultCommand) SetDefaultFlags(fs *flag.FlagSet) {
-	fs.StringVar(&cmd.destinationAddress, "rAddr", "localhost", "Remote IP-Address [default = localhost]")
-	fs.IntVar(&cmd.destinationPort, "rPort", 6969, "Remote port [default = 6969]")
+	fs.StringVar(&cmd.localAddress, "lAddr", "127.0.0.1", "Listen IP-Address [default = 127.0.0.1]")
+	fs.IntVar(&cmd.localPort, "lPort", 6969, "Listen port [default = 6969]")
 
-	fs.StringVar(&cmd.localAddress, "lAddr", "localhost", "Local IP-Address [default = localhost]")
-	fs.IntVar(&cmd.localPort, "lPort", 6969, "Local port [default = 6969]")
-
-	fs.IntVar(&cmd.maxPacketSize, "packetSize", 512, "Maximum size of a single packet [default = 512]")
-	fs.IntVar(&cmd.connectionTimeout, "timeout", 10, "LastUpdated of the connection in seconds [default = 10]")
-
-	fs.BoolVar(&cmd.debug, "debug", false, "Toggle debug mode (dumps log for every transmission)")
+	fs.IntVar(&cmd.maxPacketSize, "packetSize", 512, "Maximum size of each packet [default = 512]")
+	fs.IntVar(&cmd.connectionTimeout, "timeout", 10, "Timeout of the connection in seconds [default = 10]")
 }
 
 /*
@@ -69,13 +57,19 @@ type SendCommand struct {
 	fs *flag.FlagSet
 
 	DefaultCommand
-	filename string
+
+	destinationAddress string
+	destinationPort    int
+	filename           string
 }
 
 func NewSendCommand() *SendCommand {
 	cmd := &SendCommand{
 		fs: flag.NewFlagSet("send", flagErrorHandling),
 	}
+
+	cmd.fs.StringVar(&cmd.destinationAddress, "rAddr", "localhost", "Remote IP-Address [default = localhost]")
+	cmd.fs.IntVar(&cmd.destinationPort, "rPort", 6969, "Remote port [default = 6969]")
 	cmd.fs.StringVar(&cmd.filename, "filename", "", "The file to send")
 	return cmd
 }
@@ -99,10 +93,6 @@ func (cmd *SendCommand) Init(args []string) error {
 	return nil
 }
 
-func (cmd *SendCommand) Exec() error {
-	return nil
-}
-
 /*
 /----------------------------------------------------------------------------------------------------------------------\
 |                                                     RECEIVE-CMD                                                      |
@@ -120,7 +110,8 @@ func NewReceiveCommand() *ReceiveCommand {
 	cmd := &ReceiveCommand{
 		fs: flag.NewFlagSet("receive", flagErrorHandling),
 	}
-	cmd.fs.StringVar(&cmd.outDir, "outDir", "", "The output directory")
+
+	cmd.fs.StringVar(&cmd.outDir, "outDir", ".", "The output directory")
 	return cmd
 }
 
@@ -133,94 +124,125 @@ func (cmd *ReceiveCommand) Init(args []string) error {
 	return cmd.fs.Parse(args)
 }
 
-func (cmd *ReceiveCommand) Exec() error {
-	return nil
-}
-
 /*
 /----------------------------------------------------------------------------------------------------------------------\
 |                                                         MAIN                                                         |
 \----------------------------------------------------------------------------------------------------------------------/
 */
 
-func handleCommand(args []string) error {
-	if len(args) < 1 {
-		return errors.New("no command specified")
-	}
+var log *bufio.Writer
+var errorLog *bufio.Writer
 
-	commands := []Command{
-		NewSendCommand(),
-		NewReceiveCommand(),
+func init() {
+	logFile, err := os.Create("measure_log.txt")
+	if err != nil {
+		panic(err)
+	}
+	log = bufio.NewWriter(logFile)
+
+	errorFile, err := os.Create("error_log.txt")
+	if err != nil {
+		panic(err)
+	}
+	errorLog = bufio.NewWriter(errorFile)
+}
+
+func main() {
+	var err error
+
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Printf("%s", "not enough arguments")
+		os.Exit(-1)
 	}
 
 	selectedCommand := args[0]
+	args = args[1:]
 
-	for _, cmd := range commands {
-		if cmd.Name() == selectedCommand {
-			err := cmd.Init(args[1:])
-			if err != nil {
-				return err
-			}
-			return cmd.Exec()
+	switch selectedCommand {
+	case "send":
+		cmd := NewSendCommand()
+		err = cmd.Init(args)
+		if err != nil {
+			fmt.Printf("%v", err)
+			os.Exit(-1)
 		}
+		err = startSender(cmd)
+		if err != nil {
+			fmt.Printf("%v", err)
+			os.Exit(-1)
+		}
+		break
+	case "receive":
+		cmd := NewReceiveCommand()
+		err = cmd.Init(args)
+		if err != nil {
+			fmt.Printf("%v", err)
+			os.Exit(-1)
+		}
+		err = startReceiver(cmd)
+		if err != nil {
+			fmt.Printf("%v", err)
+			os.Exit(-1)
+		}
+	default:
+		err = fmt.Errorf("undefined command %q", selectedCommand)
+		os.Exit(-1)
 	}
 
-	return fmt.Errorf("unknown command: %s", selectedCommand)
+	fin := make(chan bool, 1)
+	<-fin
 }
 
-var log *bufio.Writer
+func startReceiver(cmd *ReceiveCommand) error {
+	lIp := cmd.localAddress
+	lPort := cmd.localPort
+	maxPacketSize := cmd.maxPacketSize
+	netTimeout := cmd.connectionTimeout
+	outPath := cmd.outDir
 
-func main() {
-	logFile, err := os.Create("go_log.txt")
-	if err != nil {
-		panic(err)
+	ip := net.ParseIP(lIp)
+	if ip == nil {
+		return fmt.Errorf("ip %q could not be parsed", lIp)
 	}
-	defer logFile.Close()
-	log = bufio.NewWriter(logFile)
 
-	remoteAddr := &net.UDPAddr{
-		IP: net.ParseIP("localhost"),
-		//IP:   net.ParseIP("10.168.239.159"),
-		Port: 6969,
-		Zone: "",
+	lAddr := &net.UDPAddr{
+		IP:   ip,
+		Port: lPort,
 	}
 
 	// Receiver
-	r, err := NewReceiver(math.MaxUint16, 10, 100000, "./down/", remoteAddr)
+	//TODO: remove buffer
+	r, err := NewReceiver(maxPacketSize, netTimeout, 100000, outPath, lAddr)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	cleaner, err := core.NewTransmissionCleaner(1, 10, &r.transmissions)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	go cleaner.Start()
 
 	// CLI
-	x, err := cli.NewCliWorker(1, &r.transmissions)
+	ui, err := cli.NewCliWorker(1, &r.transmissions)
 	if err != nil {
-		panic(err)
+		return err
 	}
-
-	commandIn := make(chan string, 10)
-	go x.Start(commandIn)
-	go func() {
-		for {
-			//TODO: handle commands
-			<-commandIn
-		}
-	}()
+	go ui.Start()
 
 	// Receiver
 	errorChannel := make(chan error, 10)
 	r.Start(errorChannel)
 	go func() {
 		for {
-			fmt.Printf("%s\n", <-errorChannel)
+			_, _ = fmt.Fprintf(errorLog, "%s\n", <-errorChannel)
 		}
 	}()
 
-	fin := make(chan bool, 1)
-	<-fin
+	return nil
+}
+
+func startSender(cmd *SendCommand) error {
+	return fmt.Errorf("%v", "sender not implemented yet")
 }
